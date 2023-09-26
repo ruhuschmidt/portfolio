591b41f7b7caa706ea3935370ddb1129f7ccd8ae
ruhuschmidt <146029288+ruhuschmidt@users.noreply.github.com>
2023-09-26 10:36:11 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+console.log('javascript');
+
+$(function() {
+$('#slideshow img:gt(0)').hide();
+setInterval(function() {
+$('#slideshow :first-child')
+.fadeOut(1000)
+.next('img')
+.fadeIn(1000)
+.end()
+
+.appendTo('#slideshow');
+}, 4000);
+});
